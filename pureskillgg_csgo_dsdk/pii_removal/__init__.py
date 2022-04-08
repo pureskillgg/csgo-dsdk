67ab46072aff7952b0635f9cc62c0348cacefb43
Bill Freeman <bill@pureskill.gg>
2022-04-08 16:55:47 -0500

    Add PII scrubber

---
+++ pureskillgg_csgo_dsdk/pii_removal/__init__.py
@@ -0,0 +1,4 @@
+"""
+Scrub PII from your data
+"""
+from .scrub_pii import scrub_pii
---
+++ pureskillgg_csgo_dsdk/pii_removal/scrub_pii_test.py
@@ -0,0 +1,39 @@
+# pylint: disable=missing-docstring
+# pylint: disable=unused-import
+
+import os
+import pytest
+from pureskillgg_dsdk import GameDsLoader, DsReaderFs
+
+from .scrub_pii import scrub_pii
+
+
+def test_remove_pii():
+    redacted = "redacted"
+    csds_reader = DsReaderFs(
+        root_path=os.path.join("fixtures"),
+        manifest_key=os.path.join("Y0C7ADW4AZEHSMxKlrFs", "csds"),
+        log=None,
+    )
+    csds_loader = GameDsLoader(reader=csds_reader, log=None)
+    data = csds_loader.get_channels(
+        [
+            {"channel": "player_name"},
+            {"channel": "header"},
+            {"channel": "player_personal"},
+            {"channel": "player_info"},
+            {"channel": "player_status", "columns": ["ping"]},
+        ]
+    )
+    data["player_info"]["commends_teacher"] = 12345
+
+    scrub_pii(data)
+    assert data["header"]["sharecode"].iat[0] == redacted
+    assert data["header"]["demo_id"].iat[0] == redacted
+    assert data["player_personal"]["clan_tag"].iat[0] == redacted
+    assert data["player_personal"]["name"].iat[0] == redacted
+    steam_ids = data["player_personal"]["steam_id"]
+    for steam_id in steam_ids:
+        assert len(steam_id) == 1
+    assert data["player_status"]["ping"].iat[100] == 0
+    assert data["player_info"]["commends_teacher"].iat[0] == 101
