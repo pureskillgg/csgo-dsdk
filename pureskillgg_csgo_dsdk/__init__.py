f4c9336e8091c77a98fd3bfd4427586192c582fe
Evan Sosenko <evan@pureskill.gg>
2022-05-05 21:08:02 -0700

    Export scrub_csds_pii

--- pureskillgg_csgo_dsdk/__init__.py
+++ pureskillgg_csgo_dsdk/__init__.py
@@ -1,6 +1,7 @@
 """
 PureSkill.gg CS:GO Data Science Development Kit.
 """
+from .scrubber import scrub_csds_pii
 from .overtime import pop_overtime
 from .errors import MissingColumns
 from .errors import UnsupportedChannelStructure
